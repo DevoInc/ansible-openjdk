59fffd6f488d3c6e71ecbfb07790debb0bc87087
Jose Manuel Usero <josemanuel.usero@devo.com>
2019-09-13 12:19:17 +0200

    add molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,26 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+import subprocess
+import pytest
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.fixture
+def get_vars(host):
+    defaults_files = "file=../../defaults/main.yml name=role_defaults"
+    vars_files = "file=../../vars/main.yml name=role_vars"
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    ansible_vars = host.ansible(
+        "include_vars",
+        defaults_files)["ansible_facts"]["role_defaults"]
+
+    ansible_vars.update(host.ansible(
+        "include_vars",
+        vars_files)["ansible_facts"]["role_vars"])
+
+    return ansible_vars
+
+
+def test_java_version(host, get_vars):
+    version = subprocess.check_output(
+        ['java', '-version'],
+        stderr=subprocess.STDOUT
+    )
+    assert str(get_vars['java__version']) in version
