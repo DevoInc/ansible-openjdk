000fb3e9944f9f3accab526e3cac442f5b7b133a
Stefan Bullones <stefan.bullones@gmail.com>
2019-09-16 17:03:18 +0200

    Fix test_java_version testinfra test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,26 +1,10 @@
-import subprocess
-import pytest
+import os
+
+import testinfra.utils.ansible_runner
 
 
-@pytest.fixture
-def get_vars(host):
-    defaults_files = "file=../../defaults/main.yml name=role_defaults"
-    vars_files = "file=../../vars/main.yml name=role_vars"
-
-    ansible_vars = host.ansible(
-        "include_vars",
-        defaults_files)["ansible_facts"]["role_defaults"]
-
-    ansible_vars.update(host.ansible(
-        "include_vars",
-        vars_files)["ansible_facts"]["role_vars"])
-
-    return ansible_vars
-
-
-def test_java_version(host, get_vars):
-    version = subprocess.check_output(
-        ['java', '-version'],
-        stderr=subprocess.STDOUT
-    )
-    assert str(get_vars['java__version']) in version
+def test_java_version(host):
+    ansible_vars = host.ansible.get_variables()
+    java_version = ansible_vars.get('java__version')
+    java_output = host.run('java -version')
+    assert str(java_version) in java_output.stderr
