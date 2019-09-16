6d93e92d912e4dda9c193f7e2bbaa861e05e0866
Stefan Bullones <stefan.bullones@gmail.com>
2019-09-16 18:42:54 +0200

    Add a . in assert, just in case

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,4 +7,4 @@ def test_java_version(host):
     ansible_vars = host.ansible.get_variables()
     java_version = ansible_vars.get('java__version')
     java_output = host.run('java -version')
-    assert str(java_version) in java_output.stderr
+    assert str(java_version) + '.' in java_output.stderr
