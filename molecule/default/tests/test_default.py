65b83058db05efcbfc0f5bf9b1e671aa7a9c01d0
Stefan Bullones <stefan.bullones@gmail.com>
2019-09-19 00:47:29 +0200

    Add openjdk11 repository for Ubuntu 16.04

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,8 +1,3 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-
 def test_java_version(host):
     ansible_vars = host.ansible.get_variables()
     java_version = ansible_vars.get('java__version')
